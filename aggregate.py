7ed64defaa1f1044b69171947cb04a20101b30ad
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2021-05-26 17:30:21 +0100

    Comment out debugging options
    These are just for printf debugging

--- aggregate.py
+++ aggregate.py
@@ -6,8 +6,11 @@ import re
 import sys
 import numpy as np
 import pandas as pd
-pd.set_option('display.max_columns', None)
-pd.set_option('display.expand_frame_repr', None)
+
+#For debugging
+#pd.set_option('display.max_columns', None)
+#pd.set_option('display.max_rows', None)
+#pd.set_option('display.expand_frame_repr', None)
 
 PREFIX='hms-nhs-the-nautical-health-service'
 DIR='doctored'
