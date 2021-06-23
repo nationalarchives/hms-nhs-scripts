87271617c7acfc0999c281da7fc22a22f0b77f4d
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2021-06-23 17:11:19 +0100

    Output according to Mimsy template
    To test: diff -qs golden_for_mimsy.csv output/for_mimsy.csv

--- templatizer.py
+++ templatizer.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
-import os
 import pandas as pd
 
 df = pd.read_csv('output/joined.csv', index_col = ['subject_id', 'task', 'volume', 'page', 'Autoresolved', 'Problems'], na_filter = False)
 
 #Assumption: each subject id maps 1:1 to a (volume, page) pair
+#TODO: I'm sure there must be a more Pandas way to do this. And it's probably less code and more efficient.
+pages = []
 for volume in df.index.unique(level = 'volume'):
-  os.mkdir(f'output/{volume}') #Will fail if volume already exists, which is what I want -- guarantees that each volume is only met once in the data
-  for page in df.index.unique(level = 'page'):
-    with open(f'output/{volume}/{page}.txt', 'x') as f:
-      lines = []
-      for index, row in df.xs((volume, page), level = ['volume', 'page']).iterrows():
-        line = []
-        for heading in df.columns:
-          value = row[heading]
-          line.append(f'{heading}, {row[heading]}')
-        lines.append('; '.join(line))
-      f.write('\n'.join(lines))
+  for page in df.xs((volume), level = 'volume').index.unique(level = 'page'):
+    lines = []
+    for index, row in df.xs((volume, page), level = ['volume', 'page']).iterrows():
+      line = []
+      for heading in df.columns:
+        value = row[heading]
+        line.append(f'{heading}, {row[heading]}')
+      lines.append('; '.join(line))
+    pages.append([f'DSH/{volume}/{page}', '. '.join(lines) + '.'])
+pd.DataFrame(pages).to_csv(path_or_buf = 'output/for_mimsy.csv', index = False, header = False)
