c95262b5dd429871d95264aa4249ad807be0899e
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2021-07-22 16:26:43 +0100

    Reduce globals
    Everything in my main loop is explicitly global, so move that into
    a subroutine to clean that up. Try to reduce global variables to
    only those that make sense as globals.

--- aggregate.py
+++ aggregate.py
@@ -17,8 +17,8 @@ from collections import defaultdict
 #pd.set_option('display.max_rows', None)
 #pd.set_option('display.expand_frame_repr', None)
 
-KEYS = ['subject_id', 'task']
-RETIREMENT_COUNT = 3 #I believe that this is the same for all workflows at all times. Can be parameterised in workflows.yaml if need be.
+bad = defaultdict(int) #Keys of bad are indices of rows in the final dataframe
+autoresolved = {} #Keys of autoresolved are indices of rows in the final dataframe
 
 parser = argparse.ArgumentParser()
 parser.add_argument('workflows',
@@ -63,18 +63,6 @@ if args.workflows == 'development_workflows':
   args.unfinished = True
   args.blanks = True
   args.no_stamp = True
-PREFIX=f'{args.exports}/hms-nhs-the-nautical-health-service'
-
-with open('workflow.yaml') as f:
-  workflow = yaml.load(f, Loader = yaml.Loader)
-
-#Read in the reduced data.
-columns = []
-bad = defaultdict(int) #Keys of bad are indices of rows in the final dataframe
-autoresolved = {} #Keys of autoresolved are indices of rows in the final dataframe
-TEXT_T = workflow['definitions']['TEXT_T']
-DROP_T = workflow['definitions']['DROP_T']
-
 
 #Recieve:
 #'candidates': A dict of category names and votes for that category
@@ -101,10 +89,13 @@ def category_resolver(candidates, threshold, subject_task, workflow_name):
 #Process data for output
 #Strings use Levenshtein distance approach, IIRC
 #Take a different approach for non-string data
-def string_resolver(row):
+def string_resolver(row, **kwargs):
+  global args, bad, autoresolved #Just being explicit that these are global
+  data = kwargs['data']
+  datacol = kwargs['datacol']
+
   if pd.isnull(row['data.consensus_score']) or pd.isnull(row['data.number_views']): return ''
 
-  global data #Just being explicit that this is a global
   if data['nptype'] == str:
     if row['data.consensus_score'] / row['data.number_views'] < args.text_threshold:
       bad[row.name] += 1
@@ -191,166 +182,182 @@ def string_resolver(row):
       return row['data.aligned_text']
   else: raise Exception()
 
+def main():
+  global args, bad, autoresolved #Be explicit that these are global
 
-for wid, data in workflow[args.workflows].items():
-  datacol = data['ztype']['name']
-  conflict_keys = []
-  reduced_file = f'{args.dir}/{data["ztype"]["type"]}_reducer_{wid}.csv'
-  if args.verbose >= 0: print(f'*** Processing {reduced_file}')
-  if data['ztype'] == TEXT_T:
-    conflict_keys = ['data.aligned_text', 'data.number_views', 'data.consensus_score']
-  try:
-    df = pd.read_csv(reduced_file,
-                     index_col = KEYS,
-                     usecols   = KEYS + [datacol] + conflict_keys,
-                     converters = {'task': lambda x: x[1:]}, #Could replace this with something that returns 1 through 25 over and over
-                     dtype     = {datacol: str})
-  except:
-    print(f'Error while reading {reduced_file}')
-    raise
+  KEYS = ['subject_id', 'task']
+  RETIREMENT_COUNT = 3 #I believe that this is the same for all workflows at all times. Can be parameterised in workflows.yaml if need be.
+  PREFIX=f'{args.exports}/hms-nhs-the-nautical-health-service'
 
-  #Handle conflicts
-  if(data['ztype'] == TEXT_T):
-    if not args.unfinished:
+  with open('workflow.yaml') as f:
+    workflow = yaml.load(f, Loader = yaml.Loader)
+
+  #Read in the reduced data.
+  columns = []
+  TEXT_T = workflow['definitions']['TEXT_T']
+  DROP_T = workflow['definitions']['DROP_T']
+
+  for wid, data in workflow[args.workflows].items():
+    datacol = data['ztype']['name']
+    conflict_keys = []
+    reduced_file = f'{args.dir}/{data["ztype"]["type"]}_reducer_{wid}.csv'
+    if args.verbose >= 0: print(f'*** Processing {reduced_file}')
+    if data['ztype'] == TEXT_T:
+      conflict_keys = ['data.aligned_text', 'data.number_views', 'data.consensus_score']
+    try:
+      df = pd.read_csv(reduced_file,
+                       index_col = KEYS,
+                       usecols   = KEYS + [datacol] + conflict_keys,
+                       converters = {'task': lambda x: x[1:]}, #Could replace this with something that returns 1 through 25 over and over
+                       dtype     = {datacol: str})
+    except:
+      print(f'Error while reading {reduced_file}')
+      raise
+
+    #Handle conflicts
+    if(data['ztype'] == TEXT_T):
+      if not args.unfinished:
+        #Drop all classifications that are based on an insufficient number of views
+        df.drop(df[df['data.number_views'] < RETIREMENT_COUNT].index)
+
+        #Report on rows with different counts
+        if args.verbose >= 1:
+          overcount = df[df['data.number_views'] > RETIREMENT_COUNT]
+          print(f'  Completed rows: {len(df.index)} (of which {len(overcount.index)} overcounted)')
+          if args.verbose >= 2 and not overcount.empty: print(overcount)
+          undercount = df[df['data.number_views'] < RETIREMENT_COUNT]
+          print(f'  Undercounted rows: {len(undercount.index)}')
+          if args.verbose >= 2 and not undercount.empty: print(undercount)
+
+      df[datacol] = df.apply(string_resolver, axis = 'columns', data = data, datacol = datacol)
+      #TODO: For these kinds of strings, may well be better to treat them like dropdowns and just take two thirds identical as permitting auto-resolve
+    elif(data['ztype'] == DROP_T):
       #Drop all classifications that are based on an insufficient number of views
-      df.drop(df[df['data.number_views'] < RETIREMENT_COUNT].index)
+      if not args.unfinished:
+        def votecounter(votes):
+          selections = ast.literal_eval(votes)
+          if len(selections) != 1: raise Exception()
+          return(sum(selections[0].values()))
+        df = df[df[datacol].apply(votecounter) >= RETIREMENT_COUNT]
 
-      #Report on rows with different counts
-      if args.verbose >= 1:
-        overcount = df[df['data.number_views'] > RETIREMENT_COUNT]
-        print(f'  Completed rows: {len(df.index)} (of which {len(overcount.index)} overcounted)')
-        if args.verbose >= 2 and not overcount.empty: print(overcount)
-        undercount = df[df['data.number_views'] < RETIREMENT_COUNT]
-        print(f'  Undercounted rows: {len(undercount.index)}')
-        if args.verbose >= 2 and not undercount.empty: print(undercount)
+        #Report on rows with different counts
+        if args.verbose >= 1:
+          overcount = df[df[datacol].apply(votecounter) > RETIREMENT_COUNT]
+          print(f'  Completed rows: {len(df.index)} (of which {len(overcount.index)} overcounted)')
+          if args.verbose >= 2 and not overcount.empty: print(overcount)
+          undercount = df[df[datacol].apply(votecounter) < RETIREMENT_COUNT]
+          print(f'  Undercounted rows: {len(undercount.index)}')
+          if args.verbose >= 2 and not undercount.empty: print(undercount)
 
-    df[datacol] = df.apply(string_resolver, axis = 'columns')
-    #TODO: For these kinds of strings, may well be better to treat them like dropdowns and just take two thirds identical as permitting auto-resolve
-  elif(data['ztype'] == DROP_T):
-    #Drop all classifications that are based on an insufficient number of views
-    if not args.unfinished:
-      def votecounter(votes):
-        selections = ast.literal_eval(votes)
+      #Process classifications for output
+      def resolver(row):
+        #row is a single-element array, containing one dictionary
+        selections = ast.literal_eval(row[datacol])
         if len(selections) != 1: raise Exception()
-        return(sum(selections[0].values()))
-      df = df[df[datacol].apply(votecounter) >= RETIREMENT_COUNT]
+        result = category_resolver(selections[0], args.dropdown_threshold, row.name, data['name'])
+        if len(result) != 1: bad[row.name] += 1
+        return str([result])
+      df[datacol] = df.apply(resolver, axis = 'columns')
+    else: raise Exception()
 
-      #Report on rows with different counts
-      if args.verbose >= 1:
-        overcount = df[df[datacol].apply(votecounter) > RETIREMENT_COUNT]
-        print(f'  Completed rows: {len(df.index)} (of which {len(overcount.index)} overcounted)')
-        if args.verbose >= 2 and not overcount.empty: print(overcount)
-        undercount = df[df[datacol].apply(votecounter) < RETIREMENT_COUNT]
-        print(f'  Undercounted rows: {len(undercount.index)}')
-        if args.verbose >= 2 and not undercount.empty: print(undercount)
+    #Tidy up columns
+    df.drop(conflict_keys, axis = 'columns', inplace = True) #Drop columns that we just brought in for conflict handling
+    df.rename(columns={datacol: data['name']}, inplace = True) #Rename the data column to something meaninful
 
-    #Process classifications for output
-    def resolver(row):
-      #row is a single-element array, containing one dictionary
-      selections = ast.literal_eval(row[datacol])
-      if len(selections) != 1: raise Exception()
-      result = category_resolver(selections[0], args.dropdown_threshold, row.name, data['name'])
-      if len(result) != 1: bad[row.name] += 1
-      return str([result])
-    df[datacol] = df.apply(resolver, axis = 'columns')
-  else: raise Exception()
+    #Convert dropdowns to their values
+    if(data['ztype'] == DROP_T):
+      labelfile = f'{args.dir}/Task_labels_workflow_{wid}_V{data["major"]}.{data["minor"]}.yaml'
+      with open(labelfile) as f:
+        labels = yaml.full_load(f)
+      def decode_dropdown(selection_json):
+          selections = ast.literal_eval(selection_json)
+          if len(selections) != 1: raise Exception()
+          selections = selections[0]
+          result = {}
+          for selection, votes in selections.items():
+            if selection == 'None': result[None] = votes
+            else:
+              label = list(labels[f'T1.selects.0.options.*.{selection}.label'].values())[0]
+              label = label[label.find('=') + 1:].strip()
+              result[label] = votes
+          if len(result) == 1:
+            return list(result.keys())[0]
+          else: return str(result)
+      df[data['name']] = df[data['name']].map(decode_dropdown)
 
-  #Tidy up columns
-  df.drop(conflict_keys, axis = 'columns', inplace = True) #Drop columns that we just brought in for conflict handling
-  df.rename(columns={datacol: data['name']}, inplace = True) #Rename the data column to something meaninful
+    columns.append(df)
 
-  #Convert dropdowns to their values
-  if(data['ztype'] == DROP_T):
-    labelfile = f'{args.dir}/Task_labels_workflow_{wid}_V{data["major"]}.{data["minor"]}.yaml'
-    with open(labelfile) as f:
-      labels = yaml.full_load(f)
-    def decode_dropdown(selection_json):
-        selections = ast.literal_eval(selection_json)
-        if len(selections) != 1: raise Exception()
-        selections = selections[0]
-        result = {}
-        for selection, votes in selections.items():
-          if selection == 'None': result[None] = votes
-          else:
-            label = list(labels[f'T1.selects.0.options.*.{selection}.label'].values())[0]
-            label = label[label.find('=') + 1:].strip()
-            result[label] = votes
-        if len(result) == 1:
-          return list(result.keys())[0]
-        else: return str(result)
-    df[data['name']] = df[data['name']].map(decode_dropdown)
-
-  columns.append(df)
-
-if args.verbose >= 0: print('*** Generating output')
-#Combine the separate workflows into a single dataframe
-#Assumption: Task numbers always refer to the same row in each workflow
-#            If this assumption does not hold, we can perform a mapping
-#            on the dataframes at the point that we read them in, above.
-#Quick test shows that this assumption does hold for now.
-first = columns.pop(0)
-joined = first.join(columns, how='outer')
+  if args.verbose >= 0: print('*** Generating output')
+  #Combine the separate workflows into a single dataframe
+  #Assumption: Task numbers always refer to the same row in each workflow
+  #            If this assumption does not hold, we can perform a mapping
+  #            on the dataframes at the point that we read them in, above.
+  #Quick test shows that this assumption does hold for now.
+  first = columns.pop(0)
+  joined = first.join(columns, how='outer')
 
 
-#Tag or remove the rows with badness
-joined.insert(0, 'Problems', '')
-joined['Problems'] = joined.apply(lambda x: 'Blank(s)' if x.isnull().values.any() else '', axis = 'columns')
-if not args.blanks:
-  incomplete_subjects = list(joined[joined.Problems != ''].index.get_level_values('subject_id').unique())
-  removed = joined.query(f'subject_id in @incomplete_subjects')
-  for key in removed.index.to_numpy():
-    bad.pop(key, None)
-    autoresolved.pop(key, None)
-  joined = joined.query(f'subject_id not in @incomplete_subjects')
+  #Tag or remove the rows with badness
+  joined.insert(0, 'Problems', '')
+  joined['Problems'] = joined.apply(lambda x: 'Blank(s)' if x.isnull().values.any() else '', axis = 'columns')
+  if not args.blanks:
+    incomplete_subjects = list(joined[joined.Problems != ''].index.get_level_values('subject_id').unique())
+    removed = joined.query(f'subject_id in @incomplete_subjects')
+    for key in removed.index.to_numpy():
+      bad.pop(key, None)
+      autoresolved.pop(key, None)
+    joined = joined.query(f'subject_id not in @incomplete_subjects')
 
-#Tag unresolved disagreements
-#TODO This part does not feel like the Pandas way
-for b in bad.keys():
-  problems = joined.at[b, 'Problems']
-  if len(problems):
-    joined.at[b, 'Problems'] = f'{problems} & {bad[b]} disagreements'
-  else:
-    joined.at[b, 'Problems'] = f'{bad[b]} disagreements'
+  #Tag unresolved disagreements
+  #TODO This part does not feel like the Pandas way
+  for b in bad.keys():
+    problems = joined.at[b, 'Problems']
+    if len(problems):
+      joined.at[b, 'Problems'] = f'{problems} & {bad[b]} disagreements'
+    else:
+      joined.at[b, 'Problems'] = f'{bad[b]} disagreements'
 
-#Record where there was autoresolution
-joined.insert(0, 'Autoresolved', '')
-#TODO: Again, doesn't feel like Pandas
-for index, value in autoresolved.items():
-  joined.at[index, 'Autoresolved'] = '; '.join(value)
-
-
-#Translate subjects ids into original filenames
-#Assumption: the metadata is invariant across all of the entries for each subject_id
-subjects = pd.read_csv(f'{PREFIX}-subjects.csv',
-                       usecols   = ['subject_id', 'metadata'])
-joined.insert(0, 'volume', '')
-joined.insert(1, 'page', '')
-for sid in joined.index.get_level_values('subject_id').unique():
-  metadata = subjects.query(f'subject_id == {sid}').iloc[0]['metadata']
-  fnam = json.loads(metadata)['Filename']
-  match = re.fullmatch('.*_(\d+)-(\d+)(?: \d)?\.jpg', fnam)
-  if match:
-    (vol, page) = [int(x) for x in match.groups()]
-    if   vol == 1: page -= 21
-    elif vol == 2: page -= 28
-    elif vol == 6:
-      print('Surprisingly met volume 6', file = sys.stderr)
-      (vol, page) = ('6', '?')
-    else: page -= 3
-  else:
-    print(f'"{fnam}" does not match regular expression', file = sys.stderr)
-    (vol, page) = '?', '?'
-  joined.loc[[sid], 'volume'] = [vol]  * 25
-  joined.loc[[sid], 'page']   = [page] * 25
+  #Record where there was autoresolution
+  joined.insert(0, 'Autoresolved', '')
+  #TODO: Again, doesn't feel like Pandas
+  for index, value in autoresolved.items():
+    joined.at[index, 'Autoresolved'] = '; '.join(value)
 
 
-#This feels ridiculous, but works in conjunction with maxcolwidth.sh to check for columns too wide for Excel
-joined.to_csv(path_or_buf = f'output/lenchecker.csv', float_format = '%.0f', sep = '@')
+  #Translate subjects ids into original filenames
+  #Assumption: the metadata is invariant across all of the entries for each subject_id
+  subjects = pd.read_csv(f'{PREFIX}-subjects.csv',
+                         usecols   = ['subject_id', 'metadata'])
+  joined.insert(0, 'volume', '')
+  joined.insert(1, 'page', '')
+  for sid in joined.index.get_level_values('subject_id').unique():
+    metadata = subjects.query(f'subject_id == {sid}').iloc[0]['metadata']
+    fnam = json.loads(metadata)['Filename']
+    match = re.fullmatch('.*_(\d+)-(\d+)(?: \d)?\.jpg', fnam)
+    if match:
+      (vol, page) = [int(x) for x in match.groups()]
+      if   vol == 1: page -= 21
+      elif vol == 2: page -= 28
+      elif vol == 6:
+        print('Surprisingly met volume 6', file = sys.stderr)
+        (vol, page) = ('6', '?')
+      else: page -= 3
+    else:
+      print(f'"{fnam}" does not match regular expression', file = sys.stderr)
+      (vol, page) = '?', '?'
+    joined.loc[[sid], 'volume'] = [vol]  * 25
+    joined.loc[[sid], 'page']   = [page] * 25
 
-#Dump output
-if not args.no_stamp:
-  remote = subprocess.run(['git', 'remote', '-v'], capture_output = True, check = True).stdout
-  joined['Repo'] = [remote] * len(joined.index)
-  commit = subprocess.run(['git', 'rev-parse', 'HEAD'], capture_output = True, check = True).stdout
-  joined['Commit'] =[commit] * len(joined.index)
-joined.to_csv(path_or_buf = f'output/{args.output}', float_format = '%.0f')
+
+  #This feels ridiculous, but works in conjunction with maxcolwidth.sh to check for columns too wide for Excel
+  joined.to_csv(path_or_buf = f'output/lenchecker.csv', float_format = '%.0f', sep = '@')
+
+  #Dump output
+  if not args.no_stamp:
+    remote = subprocess.run(['git', 'remote', '-v'], capture_output = True, check = True).stdout
+    joined['Repo'] = [remote] * len(joined.index)
+    commit = subprocess.run(['git', 'rev-parse', 'HEAD'], capture_output = True, check = True).stdout
+    joined['Commit'] =[commit] * len(joined.index)
+  joined.to_csv(path_or_buf = f'output/{args.output}', float_format = '%.0f')
+
+main()
