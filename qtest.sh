8298308211440b1d4431d6ec5cd69b24feb9b6d7
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2021-07-30 17:36:07 +0100

    Add blanks to output of qtest
    It's the only way to be sure that the data that covered all of the
    code is actually producing the right answers.
    
    I am assuming here that the current output is 'right'. It at least
    gives me a baseline to compare future changes too.

--- qtest.sh
+++ qtest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-args=(-S -u -r testinput -o joined_0.5.csv -t 0.5)
+args=(-S -u -b -r testinput -o joined_0.5.csv -t 0.5)
 
 diff -s extracttest/test18617_stdout.golden <(./clean_extraction.py extracttest/test18617.csv 18617) || { echo FAIL; exit 1; }
 diff -s extracttest/test18617.golden.csv extracttest/test18617.csv.cleaned || { echo FAIL; exit 1; }
