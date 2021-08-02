0721bc17b4cc606a2b207b717a58a254ec175769
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2021-08-02 12:18:38 +0100

    Better test setup
    Generate test data from the Panoptes exports, reducing to just the
    parts that I need. A couple of files (subjects, workflows) will
    still need to come from the user's Zooniverse download, but
    everything else can be committed.
    
    Add a test driver and a new golden file to compare against. This
    new file allows testing of more code paths than the basic test
    mode that I've been working with up until now.

---
+++ gentestinput.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir testinput/ || { echo testinput exists, delete it before running this script; exit 1; }
+
+for x in aggregation/text_reducer_*.csv; do
+  csvtool namedcol subject_id,task,data.aligned_text,data.number_views,data.consensus_score,data.consensus_text $x > testinput/`basename $x`
+done
+
+for x in aggregation/dropdown_reducer_*.csv; do
+  csvtool namedcol subject_id,task,data.value $x > testinput/`basename $x`
+done
+
+ln aggregation/Task_labels_workflow_186{1,2}4_V3.1.yaml testinput/
---
+++ qtest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+args=(launch_workflows -S -u -r testinput -o joined_0.5.csv -t 0.5)
+
+rm -rf output/; mkdir output; ./aggregate.py && diff -qs golden.csv output/joined.csv && { rm -rf output/; mkdir output; ./aggregate.py ${args[@]} && diff -s golden_0.5.csv output/joined_0.5.csv && echo PASS || false; } || { echo FAIL; false; }
+
