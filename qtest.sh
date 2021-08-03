fee4fa4b6d59c8e856e39631589b5194925a91d7
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2021-08-03 13:07:22 +0100

    qtest does not need to nuke all output
    While I'm at it, rename the output file and its comparator to
    something more obviously linked to qtest

--- qtest.sh
+++ qtest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-args=(-S -u -b -r testinput -o joined_0.5.csv -t 0.5)
+args=(-S -u -b -r testinput -o QTEST.csv -t 0.5)
 
 diff -s extracttest/test18617_stdout.golden <(./clean_extraction.py extracttest/test18617.csv 18617) || { echo FAIL; exit 1; }
 diff -s extracttest/test18617.golden.csv extracttest/test18617.csv.cleaned || { echo FAIL; exit 1; }
@@ -10,7 +10,7 @@ diff -s extracttest/test18619.golden.csv extracttest/test18619.csv.cleaned || { 
 diff -s extracttest/test18612_stdout.golden <(./clean_extraction.py extracttest/test18612.csv 18612) || { echo FAIL; exit 1; }
 diff -s extracttest/test18612.golden.csv extracttest/test18612.csv.cleaned || { echo FAIL; exit 1; }
 
-rm -rf output/; mkdir output; ./aggregate.py ${args[@]} && diff -s golden_0.5.csv output/joined_0.5.csv && ./coverage.pl ${args[@]} && echo PASS || { echo FAIL; false; }
+rm -f output/QTEST.csv; mkdir -p output; ./aggregate.py ${args[@]} && diff -s golden_QTEST.csv output/QTEST.csv && ./coverage.pl ${args[@]} && echo PASS || { echo FAIL; false; }
 
 echo 'If you want to run code coverage tool:'
 echo coverage run --branch --source=. ./aggregate.py ${args[@]}
