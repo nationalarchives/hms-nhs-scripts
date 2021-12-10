0bb18e156dde30d671cfce40e4e64ceedefe3574
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2021-12-10 17:11:37 +0000

    Initial tranche support
    * Define a file format for recording what we have read in the past.
    * Add a script to strip what we have read before from the
      extraction files.
    * Add related testing
    * Integrate into extract.sh and qtest.sh
    
    This could be improved. We could:
    * Cache the resolutions of completed columns even when the row is not complete
    * Drop all rows belonging to a single page if any rows are missing
    * Detect cases where there are extra classifications of a completed record
    
    The first two would save more compute, assuming that we do want to only
    produce complete pages for review. However, this change gives the
    minimum functionality that we need, apart from actually generating
    the views file.
    
    The final one would give us the ability to incorporate late
    classifications into the final output. However, we can also just
    check at the end by generating a new view file without stripping
    out previous tranches.

---
+++ strip_processed.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('extraction', nargs = '+')
+parser.add_argument('--tranche', '-t')
+parser.add_argument('--suffix', '-s', default = '.new')
+args = parser.parse_args()
+
+tranche_df = pd.read_csv(args.tranche, index_col = [0, 1])
+
+for extraction in args.extraction:
+  #Read in the extractions and drop all classifications relating to completed tasks
+  extraction_df = pd.read_csv(extraction, na_filter = False, index_col = False)
+  extraction_cols = list(extraction_df.columns)
+  extraction_df = extraction_df.set_index(['subject_id', 'task'])
+  extraction_df = extraction_df.join(tranche_df['complete'], how = 'left')
+  extraction_df = extraction_df[extraction_df['complete'] != True] #odd logic to handle NaN correctly
+
+  #Restore to original format.
+  #If no classifications were complete in previous tranche(s) then this leaves us with an identify transform.
+  extraction_df = extraction_df.reset_index()
+  extraction_df['tmp'] = extraction_df.apply(lambda x: int(x['task'][1:]), axis = 'columns')
+  extraction_df = extraction_df.sort_values(by = ['classification_id', 'tmp'])
+  extraction_df = extraction_df.reindex(columns = extraction_cols)
+
+  extraction_df.to_csv(path_or_buf = f'{extraction}{args.suffix}', float_format = '%.99g', index = False)
