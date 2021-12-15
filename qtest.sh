27a22e24bc8afe2b11c4889fcf03b50837014f6a
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2021-12-15 18:35:48 +0000

    Add outputs to qtest so that I know which test is running

--- qtest.sh
+++ qtest.sh
@@ -31,9 +31,11 @@ fi
 
 mkdir -p output
 
+echo "** Baseline aggregate test **"
 rm -f output/{views_,}QTEST.csv
 ./aggregate.py ${args[@]} -r testdata/baseline && diff -qs testdata/golden/golden_QTEST.csv output/QTEST.csv && diff -qs testdata/golden/golden_views_QTEST.csv output/views_QTEST.csv || { echo FAIL; exit 1; }
 
+echo "** Baseline coverage test **"
 rm -f output/{views_,}QTEST.csv
 ./coverage.pl ${args[@]} -r testdata/baseline && echo PASS || { echo FAIL; exit 1; }
 
@@ -41,33 +43,40 @@ if [ $TRANCHE -eq 0 ]; then exit; fi
 
 #repeat_row_tranche gives input as if views_QTEST.csv had been applied. The result should be output as before, but with the complete=True rows missing from QTEST.csv.
 #views_QTEST.csv should be exactly the same as before.
+echo "** Baseline tranche test **"
 rm -f output/QTEST.csv #Deliberately keep the previous views files
 ./aggregate.py ${args[@]} -r testdata/repeat_row_tranche && diff -qs testdata/golden/golden_2_QTEST.csv output/QTEST.csv && diff -qs testdata/golden/golden_views_QTEST.csv output/views_QTEST.csv || { echo FAIL; exit 1; }
 
 #additional_row_tranche adds another completed row. QTEST.csv should be as before: it happens that that completed row shows up the exact same way with these settings.
 #views_QTEST.csv should be as before, but with the new row showing complete=True
+echo "** Tranche test with extra row **"
 rm -f output/QTEST.csv #Deliberately keep the previous views file
 ./aggregate.py ${args[@]} -r testdata/additional_row_tranche && diff -qs testdata/golden/golden_2_QTEST.csv output/QTEST.csv && diff -qs testdata/golden/golden_views_2_QTEST.csv output/views_QTEST.csv || { echo FAIL; exit 1; }
 
 #Now we go back to the beginning, but now we are only reading complete pages
 #With the original data, this will result in both an empty QTEST.csv file and an empty views_QTEST.csv file
+echo "** Page tranche baseline test **"
 page_args=(-S -o QTEST.csv -t 0.5 --timing)
 rm -f output/{views_,}QTEST.csv
 ./aggregate.py ${page_args[@]} -r testdata/baseline && diff -qs testdata/golden/golden_3_QTEST.csv output/QTEST.csv && diff -qs testdata/golden/golden_views_3_QTEST.csv output/views_QTEST.csv || { echo FAIL; exit 1; }
 
 #Continue by making one page almost, but not quite, complete. Output should be the same.
+echo "** Tranche test with incomplete page **"
 rm -f output/QTEST.csv #Deliberately keep the previous views file
 ./aggregate.py ${page_args[@]} -r testdata/incomplete_page_tranche && diff -qs testdata/golden/golden_3_QTEST.csv output/QTEST.csv && diff -qs testdata/golden/golden_views_3_QTEST.csv output/views_QTEST.csv || { echo FAIL; exit 1; }
 
 #Then complete the page. Now we should get it in the output and in the views file.
+echo "** Tranche test with complete page **"
 rm -f output/QTEST.csv #Deliberately keep the previous views file
 ./aggregate.py ${page_args[@]} -r testdata/complete_page_tranche && diff -qs testdata/golden/golden_4_QTEST.csv output/QTEST.csv && diff -qs testdata/golden/golden_views_4_QTEST.csv output/views_QTEST.csv || { echo FAIL; exit 1; }
 
 #Repeat, with the effect of that views file simulated. views file should be unchanged, output should be empty again.
+echo "** Repeat tranche test with complete page **"
 rm -f output/QTEST.csv #Deliberately keep the previous views file
 ./aggregate.py ${page_args[@]} -r testdata/repeat_complete_page_tranche && diff -qs testdata/golden/golden_3_QTEST.csv output/QTEST.csv && diff -qs testdata/golden/golden_views_4_QTEST.csv output/views_QTEST.csv || { echo FAIL; exit 1; }
 
 #Complete one more page. Both outputs should now contain that page.
+echo "** Page tranche test with additional page **"
 rm -f output/QTEST.csv #Deliberately keep the previous views file
 ./aggregate.py ${page_args[@]} -r testdata/additional_page_tranche && diff -qs testdata/golden/golden_5_QTEST.csv output/QTEST.csv && diff -qs testdata/golden/golden_views_5_QTEST.csv output/views_QTEST.csv || { echo FAIL; exit 1; }
 
