23df2f7ba116c9b0af3668dcbfa3849b6b3aef5c
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2022-06-24 19:28:49 +0100

    Script to install Python deps via pip
    Removes the upper limits on the dependencies of
    panoptes_aggregation, which is risky as we have not re-tested
    panoptes_aggregation with these particular packages.
    
    Without this approach I am unable to install in a virtualenv due
    to what seems to be an ABI incompatibility between different
    numpy versions. Google phrases such as
    numpy.ndarray size changed, may indicate binary incompatibility
    for more.
    
    There is also this thread on Zooniverse Talk:
    https://www.zooniverse.org/talk/1322/2415041

---
+++ install_python_deps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+WHEEL=panoptes_aggregation-3.6.0-py3-none-any.whl
+METADATA=panoptes_aggregation-3.6.0.dist-info/METADATA
+
+trap "{ echo 'Something went wrong' >&2; rm "$WHEEL" || echo 'Unable to remove wheel' >&2; exit 1; }" ERR
+
+pip download --no-deps panoptes_aggregation==3.6.0
+unzip -p "$WHEEL" "$METADATA" |
+  sed 's#^Requires-Dist: \([^ ]\+\) ([^,]\+,#Requires-Dist: \1 (#' |
+  zip "$WHEEL" -
+echo -e '@ -\n@='"$METADATA" | zipnote -w "$WHEEL" #re https://stackoverflow.com/a/54702078
+pip install -c constraints.txt "$WHEEL" #this should stop us getting deps with different version to the stored ones
+pip freeze --exclude panoptes_aggregation       > constraints.txt
+pip freeze --exclude panoptes_aggregation --all > constraints_all.txt
+rm "$WHEEL"
