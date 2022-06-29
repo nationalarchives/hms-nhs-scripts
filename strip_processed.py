ed96d2dfe9f8e0dcb68ecc8ab2b9d01b897e8916
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2022-06-30 00:16:12 +0100

    Add multiple versions for two workflows and fix accordingly
    Actually running the code with multiple versions revealed several
    problems, which are fixed here.
    
    * aggregate.py needs to select a label file when there are multiple
      such files for a single workflow.
    * we must not concatenate extra header rows when concatenating CSV
      files
    * The strip_processed.py 'identity transform test' can fail when
      we concatenate CSV files.
    ** I assume this to be caused by rows being transcribed with
       different versions of the workflow due to a session being
       started before the workflow change, but I have not confirmed
       this. Here the problem is solved by allowing a 'do not sort'
       option to strip_processed.py.

--- strip_processed.py
+++ strip_processed.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('extraction', nargs = '+')
 parser.add_argument('--tranche', '-t')
 parser.add_argument('--suffix', '-s', default = '.new')
+parser.add_argument('--no_sort', action = 'store_true')
 args = parser.parse_args()
 
 tranche_df = pd.read_csv(args.tranche, index_col = [0, 1])
@@ -21,10 +22,13 @@ for extraction in args.extraction:
   extraction_df = extraction_df[extraction_df['complete'] != True] #odd logic to handle NaN correctly
 
   #Restore to original format.
-  #If no classifications were complete in previous tranche(s) then this leaves us with an identify transform.
+  #If no classifications were complete in previous tranche(s) and we do not sort (or if the
+  #sorting happens to be a nop, which it seems that it is when we have not concatenated extractions)
+  #then this leaves us with an identify transform.
   extraction_df = extraction_df.reset_index()
-  extraction_df['tmp'] = extraction_df.apply(lambda x: int(x['task'][1:]), axis = 'columns')
-  extraction_df = extraction_df.sort_values(by = ['classification_id', 'tmp'])
+  if not args.no_sort:
+    extraction_df['tmp'] = extraction_df.apply(lambda x: int(x['task'][1:]), axis = 'columns')
+    extraction_df = extraction_df.sort_values(by = ['classification_id', 'tmp'])
   extraction_df = extraction_df.reindex(columns = extraction_cols)
 
   extraction_df.to_csv(path_or_buf = f'{extraction}{args.suffix}', float_format = '%.99g', index = False)
