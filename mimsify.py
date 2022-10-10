82dab4bd6c3379690168553c3faa4a26d6d83afe
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2022-10-10 21:12:52 +0100

    Add a mimsifier and some tests
    The mimsifier may need some modification following discussion
    with RMG -- the main point to keep in mind for now is that I think
    that the output is more for human than for machine consumption.
    
    The tests should make it reasonably straightforward to make any
    necessary changes.

---
+++ mimsify.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+
+import re
+import csv
+import sys
+import argparse
+from datetime import datetime
+
+FIELDS = [
+  "admission number",
+  "date of entry",
+  "name",
+  "quality",
+  "age",
+  "place of birth",
+  "port sailed out of",
+  "years at sea",
+  "last services",
+  "under what circumstances admitted (or nature of complaint)",
+  "date of discharge",
+  "how disposed of",
+  "number of days victualled"
+]
+
+def normalize(row):
+  normalized_row = {}
+  for field in FIELDS:
+
+    #sanity check for expected-blank "port sailed out of"
+    if field == 'port sailed out of' and row['volume'] == '1':
+      if row[field] != '':
+        sys.stderr.write(f'Warning: non-blank "{row[field]}" in "port sailed out of" in volume 1. At row {row_count + 1} of {args.input}\n')
+
+    if not args.no_blanks_warnings:
+      if row[field] == '':
+        expected_blank = (row['volume'] == '1' and field == 'port sailed out of') or field == 'Autoresolved'
+        if not expected_blank:
+          sys.stderr.write(f'Warning: blank in "{field}" at row {row_count + 1} of {args.input} (volume {row["volume"]}, page {row["page"]})\n')
+
+    if re.match(r'[^\n\r]*[\n\r]+-{10}[\n\r]+', row[field]): #identify an unresolved field
+      if args.unresolved:
+        #Unresolved arguments are permitted, so flatten them out and remove some of the detail
+        #The thinking here is to make it as simple as possible, but we could choose to leave in some of
+        #this information, at the cost of making the entry more tricky to understand/search.
+        sub = re.sub(r'.*[\n\r]+-{10}[\n\r]+', '', row[field]) #remove the header with the "most likely" resolution
+        sub = re.sub(r' @\d+$', '', sub, flags = re.MULTILINE) #remove the count information
+        sub = re.sub(r'[\n\r]+', ' OR ', sub) #replace newlines with OR
+        normalized_row[field] = sub
+      else:
+        sys.stderr.write(f'Unresolved "{field}" at line {row_count + 1} of {args.input}\n')
+        sys.exit(1)
+    else:
+      if field == 'date of entry' or field == 'date of discharge': #convert dates to expected format
+        if row[field] != '':
+          normalized_row[field] = datetime.strptime(row[field], '%b %d %Y').strftime('%d-%m-%Y')
+        else:
+          normalized_row[field] = ''
+      else:
+        normalized_row[field] = row[field]
+
+    #This is mainly for years at sea but also applies to Autoresolved
+    if ';' in normalized_row[field]:
+      normalized_row[field] = normalized_row[field].replace(';', ':')
+      #We expect to see semicolons in years at sea and Autoresolved.
+      #Elsewhere, they might actually be in the original text, so warn about these cases.
+      #With a different "mimsy separator" we would not have this problem
+      if field != 'years at sea' and field != 'Autoresolved':
+        sys.stderr.write(f'Warning: replaced semicolons in "{row[field]}" (See "{field}" at row {row_count + 1} of {args.input})\n')
+  return normalized_row
+
+def next_page():
+  if page_count -1 == args.pages:
+    print(f'Stopped at {page_count - 1} pages')
+    sys.exit(0)
+  target.write(f'DSH/{row["volume"]}/{row["page"]},')
+  return row['volume'], row['page']
+      
+def next_row():
+  normalized_row = normalize(row)
+  target.write('; '.join([f'{field}, {normalized_row[field]}' for field in FIELDS]))
+  target.write(';')
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('input',
+                    nargs = '?',
+                    default = 'output/joined.csv',
+                    help = 'Input file (default: output/joined.csv)')
+parser.add_argument('--output', '-o',
+                    default = 'output/mimsy.txt',
+                    help = 'Output file (default: output/mimsy.txt)')
+parser.add_argument('--skip', '-s',
+                    nargs = '*',
+                    default = [],
+                    help = 'A field to skip. For example, --skip "years at sea" will leave out the "years at sea" field. Use --list to see all fields.')
+parser.add_argument('--list', '-l',
+                    action = 'store_true',
+                    help = 'Apply any skips, list fields and exit')
+parser.add_argument('--unresolved',
+                    action = 'store_true',
+                    help = 'Permit (and flatten) unresolved fields')
+parser.add_argument('--no-blanks-warnings',
+                    action = 'store_true',
+                    help = 'Do not warn about unexpected blanks')
+parser.add_argument('--autoresolved',
+                    action = 'store_true',
+                    help = 'Include information about which fields were autoresolved')
+parser.add_argument('--pages',
+                    type = int,
+                    help = 'Stop after PAGES pages (for faster testing)')
+args = parser.parse_args()
+
+FIELDS = list(filter(lambda x: x not in args.skip, FIELDS))
+if args.list:
+  print('\n'.join(FIELDS))
+  sys.exit(0)
+if args.autoresolved:
+  FIELDS.append('Autoresolved')
+
+with open(args.input) as source:
+  #QUOTE_MINIMAL is the default, but let's be explicit about what we expect
+  #This is on the basis that Google Sheets appears to export as QUOTE_MINIMAL
+  reader = csv.DictReader(source, quoting = csv.QUOTE_MINIMAL, strict = True)
+  page_count = 1
+  row_count = 1
+
+  try:
+    with open(args.output, 'w') as target:
+      row = next(reader)
+      current_volume, current_page = next_page()
+      next_row()
+
+      for row in reader:
+        #This conditional relies upon volumes and pages appearing in order,
+        #which is how aggregate.py happens to organise output/joined.csv.
+        if row['page'] != current_page or row['volume'] != current_volume:
+          page_count += 1
+          target.write('\n')
+          current_volume, current_page = next_page()
+        else: target.write(' ') #so that the rows are separated the same way as the fields are
+        row_count += 1
+        next_row()
+
+      target.write('\n') #Put in a trailing newline, if only to make testing simpler
+  except:
+    sys.stderr.write(f'Exception while parsing row {row_count + 1} of {args.input}\n\n')
+    raise
---
+++ test_mimsy/test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+PASSCOUNT=0
+FAILCOUNT=0
+
+function diff_test {
+  local testcase="$1"
+  shift
+  ../mimsify.py "$@" -o output/"$testcase".txt input/"$testcase".csv 2>&1 | diff -q - expected/"$testcase".err > /dev/null 2>&1
+  if [[ $? -ne 0 ]]; then
+    echo "FAIL (bad messages) $testcase $@"
+    ((FAILCOUNT++))
+    return
+  fi
+  diff -q expected/"$testcase".txt output/"$testcase".txt > /dev/null
+  if [[ $? -eq 0 ]]; then
+    echo "PASS  diff_test $testcase $@"
+    ((PASSCOUNT++))
+  else
+    echo "FAIL (bad output) diff_test $testcase $@"
+    ((FAILCOUNT++))
+  fi
+}
+
+BASEDIR="`dirname $0`"
+cd $BASEDIR
+mkdir output || { echo "`realpath ${BASEDIR}`/output already exists: please remove and retry" 2>&1; exit 1; }
+
+diff_test good_page #test basic functionality. this also tests that blanks are accepted in "port sailed out of" when "volume" is 1
+diff_test unresolveds_rej #test that unresolveds are rejected. this one fails, so it is fine that there is a trailing space and no terminating line ending
+diff_test unresolveds_acc --unresolved #test that unresolveds are correctly handled when we accept them
+diff_test two_pages --unresolved #test two pages together (we use unresolved here just to let us combine the previous tests into two pages)
+diff_test bad_blanks_vol_1 #make sure that a blank outside of "port sailed out of" is picked up in vol 1
+diff_test bad_port_vol_1 #make sure that non-blank "port sailed out of" with vol 1 raises a warning
+diff_test bad_blanks #make sure that blanks anywhere in not-vol-1 raise a warning
+diff_test auto_page --autoresolved #check that autoresolved correctly adds autoresolved fields. Also tests that ; conversion and non-reporting of expected blank field works correctly
+diff_test skip_yas --skip "years at sea" #make sure that we can skip the field that this option was added for
+diff_test skip_no --skip "admission number" #make sure nothing surprising happens if we skip the first field
+diff_test skip_victualled --skip "number of days victualled" #make sure nothing surprising happens if we skip the final field
+diff_test skip_two --skip "date of entry" "quality" #make sure we can skip more than one field
+diff_test skip_neighbours --skip "age" "place of birth" #make sure nothing surprising happens if we remove adjacent columns
+
+#If everything passes, test the whole of phase one. The reference output has not been
+#checked for correctness, the point of this one is just to catch inadvertent changes.
+if [[ $FAILCOUNT -eq 0 ]]; then diff_test phase_one --unresolved; fi
+
+echo "Passed $PASSCOUNT of $((PASSCOUNT + FAILCOUNT)) tests"
+exit $FAILCOUNT
