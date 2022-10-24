ea52d79f9ac7eb5fa9035e80dc1c11fecda3a571
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2022-10-24 11:47:51 +0100

    Clean up maxcolwidth.sh output

--- misc_scripts/maxcolwidth.sh
+++ misc_scripts/maxcolwidth.sh
@@ -11,13 +11,14 @@ else
   #Following https://unix.stackexchange.com/a/92149
   high=`sed 's/\^/\n/g' "${checkdir}"/lenchecker.csv | awk '{ print length }' | sort -g | tail -1`
   if [ ${high} -gt 49000 ]; then
-    echo "${high} is greater than Google Sheets max of 50,000" >&2
-    echo '{The Sheets max is 50,000, but giving myself a buffer, just in case)' >&2
-  elif [ ${high} -gt 250 ]; then
-    echo "${high} is greater than Excel max of 250" >&2
-    echo '(The Excel max is 255, but giving myself a small buffer, just in case)' >&2
+    echo -n "${high} is greater than Google Sheets max of 49,000"
+    echo ' {The Sheets max is 50,000, but giving myself a buffer, just in case)'
     false
+  elif [ ${high} -gt 250 ]; then
+    echo "${high} is less than Google Sheets max of 50,000"
+    echo -n "${high} is greater than Excel max of 250"
+    echo ' (The Excel max is 255, but giving myself a small buffer, just in case)'
   else
-    echo "${high} should be OK as max col width"
+    echo "${high} should be OK as max col width in all tested cases"
   fi
 fi
